0b6e2fb76dee2077dacf450e673120fb834101ef
AsuncionRivera <32930725+AsuncionRivera@users.noreply.github.com>
2018-01-21 14:38:54 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-#Merges the training and the test sets to create one data set.
+#1. Merges the training and the test sets to create one data set.
 
 to_be_merged1 <- read.table("train/subject_train.txt")            #reads the table subject_train.txt
 to_be_merged2 <- read.table("test/subject_test.txt")              #reads the table subject_text.txt
@@ -15,15 +15,15 @@ to_be_merged6 <- read.table("test/y_test.txt")                    #reads y_train
 merged_y <- rbind(to_be_merged3, to_be_merged4)                   #merges y_text.txt and y_train.txt
 head(merged_y)                                                    #checks the merged y
 
-#Extracts only the measurements on the mean and standard deviation for each measurement.
+#2. Extracts only the measurements on the mean and standard deviation for each measurement.
 features <- read.table("features.txt")                            #reads the table features.txt
 #Sorry I can't get the mean and standard deviation
 
-#Uses descriptive activity names to name the activities in the data set
+#3. Uses descriptive activity names to name the activities in the data set
 activitiy_labels<- read.table("activity_labels.txt")              #reads the table activity_labels.txt
 #Sorry I'm unable to do this
 
-#Appropriately labels the data set with descriptive variable names.
+#4. Appropriately labels the data set with descriptive variable names.
 names(merged_subject) <- "subject"                                #labels merged_subject with "subject"
 names(merged_X) <- "set"                                          #labels merged_X with "set" for training and test set
 names(merged_y) <- "labels"                                       #labels merged_y with "labels" for training and test labels
@@ -31,4 +31,4 @@ names(merged_y) <- "labels"                                       #labels merged
 merged <- cbind(merged_subject, merged_X, merged_y)               #merges merged_subject, merged_X, merged_y
 write.table(merged, "merged_data_set.txt")                        #writes the merged data into a new data set
 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
