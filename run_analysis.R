694a6feae5a1875297858f1278c058b76a0f0784
AsuncionRivera <32930725+AsuncionRivera@users.noreply.github.com>
2018-01-21 10:16:04 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,14 @@
 #Merges the training and the test sets to create one data set.
 
-to_be_merged1 <- read.table("train/subject_train.txt")        #reads the table subject_train.txt
+to_be_merged1 <- read.table("train/subject_train.txt")            #reads the table subject_train.txt
 to_be_merged2 <- read.table("test/subject_test.txt")              #reads the table subject_text.txt
-merged_subject <- rbind(to_be_merged1, to_be_merged2)
+merged_subject <- rbind(to_be_merged1, to_be_merged2)             #merges subject_train.txt and subject_text.txt
+head(merged_subject)                                              #checks the merged subject
 
-to_be_merged3 <- read.table("test/X_test.txt")
-to_be_merged4 <- read.table("train/X_train.txt")
-merged_X <- rbind(to_be_merged3, to_be_merged4)
+to_be_merged3 <- read.table("test/X_test.txt")                    #reads X_text.txt
+to_be_merged4 <- read.table("train/X_train.txt")                  #reads X_train.txt
+merged_X <- rbind(to_be_merged3, to_be_merged4)                   #merges X_text.txt and X_train.txt
+head(merged_X)                                                    #checks the merged X
 
 to_be_merged5
 to_be_merged6
